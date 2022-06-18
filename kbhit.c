56a7e4593d4cc237767964e871a74826b1f4682d
Muhammad Naqdi <didi@debian-pc>
2022-06-19 02:34:02 +0430

    Add files

---
+++ kbhit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+char kbhit() {
+	struct termios old_tio, new_tio;
+	char c;
+
+	tcgetattr(STDIN_FILENO, &old_tio);
+	new_tio = old_tio;
+
+	new_tio.c_lflag &=(~ICANON & ~ECHO);
+
+	tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+	
+	c = getchar();
+	
+	tcsetattr(STDIN_FILENO, TCSANOW,&old_tio);
+	
+	return c;
+}
